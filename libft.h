efe56c9887904ab9e4c93bcc7cc8493fe81e9d3a
root <root@DESKTOP-MJLJHQU>
2024-10-14 14:12:08 -0300

    libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 13:44:32 by root              #+#    #+#             */
-/*   Updated: 2024/10/14 13:51:32 by root             ###   ########.fr       */
+/*   Updated: 2024/10/14 14:11:29 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,6 @@ int	ft_isalpha(int c);
 int	ft_isascii(int c);
 int	ft_isalnum(int c);
 int	ft_isprint(int c);
+size_t ft_strlen(const char *s);
 
 #endif
