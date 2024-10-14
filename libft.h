cbf07c2462d326c0e4a469f73c0cc81c6aaa1277
root <root@DESKTOP-MJLJHQU>
2024-10-14 13:51:55 -0300

    libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 13:44:32 by root              #+#    #+#             */
-/*   Updated: 2024/10/14 13:46:02 by root             ###   ########.fr       */
+/*   Updated: 2024/10/14 13:51:32 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,11 @@
 # define LIBFT_H
 
 # include <stddef.h>
+
+int	ft_isdigit(int c);
+int	ft_isalpha(int c);
+int	ft_isascii(int c);
+int	ft_isalnum(int c);
+int	ft_isprint(int c);
 
 #endif
