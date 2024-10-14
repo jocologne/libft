032fc7b7dad90323e109137b66034f43e4e779ce
root <root@DESKTOP-MJLJHQU>
2024-10-14 19:25:25 -0300

    savepoint

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: jcologne <jcologne@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 17:17:21 by jcologne          #+#    #+#             */
-/*   Updated: 2024/10/14 18:39:35 by jcologne         ###   ########.fr       */
+/*   Updated: 2024/10/14 19:25:17 by jcologne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,6 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 		ft_memcpy (dst, src, srclen + 1);
 	else
 		ft_memcpy(dst, src, srclen - 1);
+		dst[dstsize - 1] = 0;
 	return (srclen);
 }
