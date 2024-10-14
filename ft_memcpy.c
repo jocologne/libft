41b9998e3aee8947681707c9e8b10d2dd1c8eeda
root <root@DESKTOP-MJLJHQU>
2024-10-14 16:43:58 -0300

    savepoint

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,15 +6,19 @@
 /*   By: jcologne <jcologne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 16:39:08 by jcologne          #+#    #+#             */
-/*   Updated: 2024/10/14 16:39:34 by jcologne         ###   ########.fr       */
+/*   Updated: 2024/10/14 16:43:51 by jcologne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "stddef.h"
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	char	*tmp_dest;
 	char	*tmp_src;
 
+	if (dest == (void *)0 && src == (void *)0)
+		return (dest);
 	while (n > 0)
 	{
 		tmp_dest = (char *)dest;
