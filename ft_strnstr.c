112a8a5c24965c26f568bbbde35e64c3cff1a720
João <jocolognesi@gmail.com>
2024-10-29 02:23:25 -0300

    commit

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,28 +6,28 @@
 /*   By: jcologne <jcologne@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 20:10:46 by jcologne          #+#    #+#             */
-/*   Updated: 2024/10/18 15:25:21 by jcologne         ###   ########.fr       */
+/*   Updated: 2024/10/29 02:16:52 by jcologne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *s1, const char *s2, size_t len)
+char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
 	size_t	i;
 	size_t	j;
 
 	i = 0;
 	j = 0;
-	if (*s2 == 0)
-		return ((char *)s1);
-	while (s1[i] && i < len)
+	if (*little == 0)
+		return ((char *)big);
+	while (big[i] && i < len)
 	{
-		while (s1[i + j] == s2[j] && s1[i + j] && i + j < len)
+		while (big[i + j] == little[j] && big[i + j] && i + j < len)
 		{
 			j++;
-			if (s2[j] == '\0')
-				return ((char *) s1 + i);
+			if (little[j] == '\0')
+				return ((char *) big + i);
 		}
 		i++;
 		j = 0;
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jcologne <jcologne@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 13:44:32 by root              #+#    #+#             */
-/*   Updated: 2024/10/29 02:03:11 by jcologne         ###   ########.fr       */
+/*   Updated: 2024/10/29 02:18:43 by jcologne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
-char	*ft_strnstr(const char *s1, const char *s2, size_t len);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
 int		ft_atoi(const char *nptr);
 char	*ft_strdup(const char *s);
 void	*ft_calloc(size_t num, size_t size);
